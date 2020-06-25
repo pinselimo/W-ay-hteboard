d7d77c7e5d2cb2fe2d2ab251f2311fbad422e8c1
Simon Plakolb <s.plakolb@gmail.com>
2020-06-26 01:26:07 +0200

    Remove unnecessary debugging features

--- cc-drawing-area.c
+++ cc-drawing-area.c
@@ -124,7 +124,6 @@ cc_drawing_area_event (GtkWidget *widget,
 	GdkInputSource source;
 	GdkDeviceTool *tool;
 	GdkDevice *device;
-    g_debug("THERE");
 	device = gdk_event_get_source_device (event);
 
 	if (!device)
@@ -168,8 +167,7 @@ cc_drawing_area_event (GtkWidget *widget,
 		cairo_move_to (area->cr, x, y);
 
 		gtk_widget_queue_draw (widget);
-        g_debug("HRE");
-        return GDK_EVENT_PROPAGATE;//GDK_EVENT_STOP;
+        return GDK_EVENT_STOP;
 	}
 
 	return GDK_EVENT_PROPAGATE;
@@ -195,7 +193,6 @@ cc_drawing_area_init (CcDrawingArea *area)
 			       GDK_BUTTON_PRESS_MASK |
 			       GDK_BUTTON_RELEASE_MASK |
 			       GDK_POINTER_MOTION_MASK);
-    g_debug("Initialized");
 }
 
 GtkWidget *
